e38bcfe78df395fe19c1d03be85c6048513054ff
Alexander Orlov <kimase1@yandex.ru>
2024-06-25 20:11:07 +0300

    [F] to my repo

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-      .package(url: "https://github.com/littleTurnip/FFmpegKit.git", branch: "main"),
+      .package(url: "https://github.com/Anonym0uz/FFmpegKit.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
